914ca5de89bc895eb1208a366a70cf1a8d98b874
Mike Duke <aithon@gmail.com>
2018-08-27 18:51:32 -0600

    Error message on empty field

--- main.js
+++ main.js
@@ -7,22 +7,28 @@ var linkDisplay = document.querySelector('.link-display');
 linkTitle.focus();
 
 inputButton.addEventListener('click', function() {
+  if (linkTitle.value === '' || linkURL.value === '') {
+    event.preventDefault();
+    alert('Please enter a site name and URL');
+  }
+  
   var newCard = new Card(linkTitle, linkURL);
   addCard(newCard);
   clearInputs();
   inputButton.disabled = true;
+
 })
 
 linkTitle.addEventListener('keyup', function() {
   if (linkTitle.value !== '' && linkURL.value !== '') {
     inputButton.disabled = false;
-  }
+  } 
 })
 
 linkURL.addEventListener('keyup', function() {
   if (linkTitle.value !== '' && linkURL.value !== '') {
     inputButton.disabled = false;
-  }
+  } 
 })
 
 linkURL.addEventListener('paste', function() {
